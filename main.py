3fa6139dd00fb0af9f226a5753106010856b2550
Justin <meimar@ualberta.ca>
2025-03-27 17:58:43 -0600

    impl: start interference graph

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+from typing import List
 
 class ProgramPoint:
     def __init__(self):
@@ -20,13 +21,18 @@ class Register:
     def __init__(self, name):
         self.name = name
         self.value = 0
+        self.is_live = False
+    
+    def is_live(self):
+        return self.is_live
 
     def __str__(self):
         return f"[{self.name}:{self.value}]"
 
 class RegisterSet():
     def __init__(self, cap):
-        self.cap = cap  # default 4
+        self.cap = cap
+        # name the registers r<I> for I in 0..n
         self.registers = [Register("r"+str(i)) for i in range(0, self.cap)]
 
     def get_capacity(self) -> int:
@@ -38,9 +44,34 @@ class RegisterSet():
             string += f"- {str(r)}\n"
         return string
 
+class InterferenceGraph():
+    def __init__(self, universe_size: int):
+        # use adjacency matrix
+        self.adj_matrix = [] 
+        for i in range(universe_size):
+            row = [0 for _ in range(universe_size)]
+            self.graph.append(row)
+    
+    def __str__(self):
+        string = ""
+        for row in self.adj_matrix:
+            string += str(row)
+            string += "\n"
+        return string
+
+class Solver():
+
+    def __init__(reg_set: RegisterSet, points: List[ProgramPoint]):
+        self.reg_set = reg_set 
+        self.points = points
+    
+    def register_coloring(self):
+        for point in self.points:
+            pass             
+    
 def parse_input(path):
     """
-
+    Read the problem input from an input file.
     """
     with open(path, "r") as input_file:
         n_reg = input_file.readline()
@@ -67,4 +98,5 @@ if __name__ == "__main__":
     print(register_set)
     for point in program_points:
         print(point)
+    
 
