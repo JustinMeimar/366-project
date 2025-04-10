4c67b2584499567cab9f14942d75119f11c32f20
Justin <meimar@ualberta.ca>
2025-04-10 12:07:34 -0600

    improve plot

--- scripts/scatter.py
+++ scripts/scatter.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import random
 
 df = pd.read_csv('./bench.csv')
+
 df['ProblemID'] = df.apply(
     lambda x: f"{x['NumVariables']}_{x['NumSpilled']}_{x['NumEdges']}", 
     axis=1
@@ -10,8 +12,6 @@ df['ProblemID'] = df.apply(
 
 greedy_times = []
 backtrack_times = []
-greedy_colors = []
-backtrack_colors = []
 
 for problem_id in df['ProblemID'].unique():
     problem_data = df[df['ProblemID'] == problem_id]
@@ -27,12 +27,8 @@ for problem_id in df['ProblemID'].unique():
         
     greedy_times.append(greedy_row['TimeToSolve'].values[0])
     backtrack_times.append(backtrack_row['TimeToSolve'].values[0])
-    greedy_colors.append(greedy_row['MaxColorsUsed'].values[0])
-    backtrack_colors.append(backtrack_row['MaxColorsUsed'].values[0])
 
-plt.figure(figsize=(10, 5))
-
-plt.subplot(1, 2, 1)
+plt.figure(figsize=(10, 8))
 plt.scatter(greedy_times, backtrack_times, color='green', alpha=0.7)
 plt.xlabel('Greedy Time (s)')
 plt.ylabel('Backtracking Time (s)')
@@ -45,16 +41,5 @@ max_time = max(max(greedy_times), max(backtrack_times))
 plt.loglog([min_time, max_time], [min_time, max_time], 'k-')
 plt.grid(True)
 
-plt.subplot(1, 2, 2)
-plt.scatter(greedy_colors, backtrack_colors, color='green', alpha=0.7)
-plt.xlabel('Greedy Max Colors')
-plt.ylabel('Backtracking Max Colors')
-plt.title('Max Colors Used Comparison')
-
-max_colors = max(max(greedy_colors), max(backtrack_colors))
-plt.plot([0, max_colors], [0, max_colors], 'k-')
-plt.grid(True)
-
 plt.tight_layout()
-# plt.show()
-plt.savefig("./color_scattern.pn")
+plt.savefig("./time_comparison.png")
